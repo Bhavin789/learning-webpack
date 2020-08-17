7bdf114c2f9f0d560a864c8a038049019016df33
Bhavin Agarwal <bhavib.789@gmail.com>
2020-08-17 23:21:01 +0530

    Add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    contentBase: "./dist",
+  },
   plugins: [new HtmlWebpackPlugin({ title: "Output Management" })],
   module: {
     rules: [
